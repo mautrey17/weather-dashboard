913a1dd8d69d17917a0b588e07c1017d99fd93c5
Matt Autrey <mautrey17@gmail.com>
2021-01-13 21:46:04 -0500

    added image to current city

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,21 +6,27 @@ var show = false
 function getWeather() {
     // city = "raleigh";
     var currentWeatherQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=56ecce8b9a89bb783f4aaec43e6b84a7&units=imperial";
-    var weatherIcon = response.weather[0].icon;
-    var imgQuery = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
+    var weatherIcon;
+    
+    
     
     $(".current-weather").empty();
     
     $.get(currentWeatherQuery).then(function(response){
         console.log(response);
 
+        weatherIcon = response.weather[0].icon;
+        var imgQuery = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+
+        console.log(imgQuery);
+
         //create location vars for other calls
         var lat = response.coord.lat;
         var lon = response.coord.lon;
         //create HTML elements
-        var cityDisplay = $("<h2>").text(response.name + " " + dayjs().format("MM/DD/YYYY") + " picHere").addClass("card-title")
+        var cityDisplay = $("<h2>").text(response.name + " " + dayjs().format("MM/DD/YYYY")).addClass("card-title")
         var iconSpan = $("<span>");
-        var currentImg = $("img").attr("src", imgQuery)
+        var currentImg = $("<img>").attr("src", imgQuery)
         var currentTemp = $("<p>").text("Tempurature: " + response.main.temp);
         var currentHumidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
         var currentWind = $("<p>").text("Windspeed: " + response.wind.speed + " MPH");
